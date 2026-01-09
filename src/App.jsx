b21e66210352a42ee9d72fd4c3a058113acb5be6
Serena <sere.michetti@icloud.com>
2026-01-09 12:47:06 +0100

    creato prima parte di header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
 function App() {
 
 
   return (
-    <div>
-      <p>ciao</p>
+    <div id='app'>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
 
-export default App
+export default Header
