aef01b9a1ceddbb5107d0a17ef58e2aeb8d39d9e
Serena <sere.michetti@icloud.com>
2026-01-26 19:28:17 +0100

    creata seconda sezione del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,60 @@ function Footer() {
 
     return (
         <footer>
-            <section id="merchandise">
-                <ul>
-                    <li><img  src= "/buy-comics-digital-comics.png" alt= "digital comics"/>DIGITAL COMICS</li>
-                    <li><img  src= "/buy-comics-merchandise.png" alt= "dc object"/>DC MERCHANDISE</li>
-                    <li><img  src= "/buy-comics-subscriptions.png" alt= "dc subsciption"/>SUBCRIPTION</li>
-                    <li><img  src= "/buy-comics-shop-locator.png" alt= "dc locator"/>COMIC SHOP LOCATOR</li>
-                    <li><img  src= "/buy-dc-power-visa.svg" alt= "dc locator"/>DC POWER VISA</li>
-                </ul>
-            </section>
+            <ul id="merchandise">
+                <li><img src="/buy-comics-digital-comics.png" alt="digital comics" />DIGITAL COMICS</li>
+                <li><img src="/buy-comics-merchandise.png" alt="dc object" />DC MERCHANDISE</li>
+                <li><img src="/buy-comics-subscriptions.png" alt="dc subsciption" />SUBCRIPTION</li>
+                <li><img src="/buy-comics-shop-locator.png" alt="dc locator" />COMIC SHOP LOCATOR</li>
+                <li><img src="/buy-dc-power-visa.svg" alt="dc locator" />DC POWER VISA</li>
+            </ul>
+            <div id="info">
+                <div className="column left">
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul>
+                        <h3>SHOP</h3>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                    <ul>
+                        <h3>DC</h3>
+                        <li>Tem Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscription</li>
+                        <li>Talent Workshop</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                    <ul>
+                        <h3>SITES</h3>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+                <div id="bg-dc-logo" className="column right">
+
+                </div>
+            </div>
+            <div id="social">
+                terza sezione
+            </div>
         </footer>
 
     );
