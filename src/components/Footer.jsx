ad2a3afa4589379351d0b7820e9e9af388e4f906
Serena <sere.michetti@icloud.com>
2026-01-26 18:02:20 +0100

    creata prima sezione del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ function Footer() {
 
     return (
         <footer>
-            contenuto footer
+            <section id="merchandise">
+                <ul>
+                    <li><img  src= "/buy-comics-digital-comics.png" alt= "digital comics"/>DIGITAL COMICS</li>
+                    <li><img  src= "/buy-comics-merchandise.png" alt= "dc object"/>DC MERCHANDISE</li>
+                    <li><img  src= "/buy-comics-subscriptions.png" alt= "dc subsciption"/>SUBCRIPTION</li>
+                    <li><img  src= "/buy-comics-shop-locator.png" alt= "dc locator"/>COMIC SHOP LOCATOR</li>
+                    <li><img  src= "/buy-dc-power-visa.svg" alt= "dc locator"/>DC POWER VISA</li>
+                </ul>
+            </section>
         </footer>
 
     );
